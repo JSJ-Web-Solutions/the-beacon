78cc2880c439921c247b83d5ddc6a4c392c5ac08
sleklere <santiagoleklere@gmail.com>
2024-06-15 22:55:34 -0300

    quest items refactored

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -2,6 +2,13 @@ import QuestModal from "../components/QuestModal";
 import Icon from "../assets/checkpoint-icon-large.png";
 import MiniIcon from "../assets/checkpoint-icon-sm-2.png";
 import MiniIconDark from "../assets/checkpoint-icon-sm-1.png";
+import data from "../assets/questItems.json";
+
+const iconComponents = {
+  Icon: Icon,
+  MiniIcon: MiniIcon,
+  MiniIconDark: MiniIconDark,
+};
 
 function Event() {
   return (
@@ -9,205 +16,19 @@ function Event() {
       <div>
         <div className="event-map_main-container">
           <div className="event-map_top-container">
-            <div className="event-map_top-container--rectangle event-map_top-container--rectangle_first">
-              <div></div>
-            </div>
-            <div className="event-map_top-container--rectangle_child event-map_top-container--rectangle_child__first">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_top-container--rectangle event-map_top-container--rectangle_second">
-              <div></div>
-            </div>
-            <div className="event-map_top-container--rectangle_child event-map_top-container--rectangle_child__second">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_top-container--rectangle event-map_top-container--rectangle_third">
-              <div></div>
-            </div>
-            <div className="event-map_top-container--rectangle_child event-map_top-container--rectangle_child__third">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_top-container--rectangle event-map_top-container--rectangle_fourth">
-              <div></div>
-            </div>
-            <div className="event-map_top-container--rectangle_child event-map_top-container--rectangle_child__fourth">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_top-container--rectangle event-map_top-container--rectangle_fifth">
-              <div></div>
-            </div>
-            <div className="event-map_top-container--rectangle_child event-map_top-container--rectangle_child__fifth">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_top-container--rectangle event-map_top-container--rectangle_sixth">
-              <div></div>
-            </div>
-            <div className="event-map_top-container--rectangle_child event-map_top-container--rectangle_child__sixth">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_top-container--rectangle event-map_top-container--rectangle_seventh">
-              <div></div>
-            </div>
-            <div className="event-map_top-container--rectangle_child event-map_top-container--rectangle_child__seventh">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-          </div>
-
-          <div className="event-map_bottom-container">
-            <div className="event-map_bottom-container--rectangle event-map_bottom-container--rectangle_first">
-              <div></div>
-            </div>
-            <div className="event-map_bottom-container--rectangle_child event-map_bottom-container--rectangle_child__first">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_bottom-container--rectangle event-map_bottom-container--rectangle_second">
-              <div></div>
-            </div>
-            <div className="event-map_bottom-container--rectangle_child event-map_bottom-container--rectangle_child__second">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_bottom-container--rectangle event-map_bottom-container--rectangle_third">
-              <div></div>
-            </div>
-            <div className="event-map_bottom-container--rectangle_child event-map_bottom-container--rectangle_child__third">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_bottom-container--rectangle event-map_bottom-container--rectangle_fourth">
-              <div></div>
-            </div>
-            <div className="event-map_bottom-container--rectangle_child event-map_bottom-container--rectangle_child__fourth">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_bottom-container--rectangle event-map_bottom-container--rectangle_fifth">
-              <div></div>
-            </div>
-            <div className="event-map_bottom-container--rectangle_child event-map_bottom-container--rectangle_child__fifth">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_bottom-container--rectangle event-map_bottom-container--rectangle_sixth">
-              <div></div>
-            </div>
-            <div className="event-map_bottom-container--rectangle_child event-map_bottom-container--rectangle_child__sixth">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_bottom-container--rectangle event-map_bottom-container--rectangle_seventh">
-              <div></div>
-            </div>
-            <div className="event-map_bottom-container--rectangle_child event-map_bottom-container--rectangle_child__seventh">
-              <QuestModal
-                size={60}
-                icon={MiniIconDark}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
+            {data.itemsTop.map((item, index) => {
+              return (
+                <div className={item.className}>
+                  <QuestModal
+                    size={item.size}
+                    icon={iconComponents[item.icon]}
+                    title={item.title}
+                    text={item.text}
+                    video={item.videoName}
+                  />
+                </div>
+              );
+            })}
           </div>
 
           <div className="event-map_right-line"></div>
@@ -215,101 +36,35 @@ function Event() {
 
           <div className="event-map_center-line">
             <div className="event-map_center-line--rectangle event-map_center-line--rectangle_circle"></div>
-            <div className="event-map_center-line--rectangle event-map_center-line--rectangle_first">
-              <QuestModal
-                size={80}
-                icon={Icon}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_center-line--rectangle event-map_center-line--rectangle_second">
-              {" "}
-              <QuestModal
-                size={80}
-                icon={Icon}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_center-line--rectangle event-map_center-line--rectangle_third">
-              {" "}
-              <QuestModal
-                size={80}
-                icon={Icon}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_center-line--rectangle event-map_center-line--rectangle_fourth">
-              {" "}
-              <QuestModal
-                size={80}
-                icon={Icon}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_center-line--rectangle event-map_center-line--rectangle_fifth">
-              {" "}
-              <QuestModal
-                size={80}
-                icon={Icon}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_center-line--rectangle event-map_center-line--rectangle_sixth">
-              {" "}
-              <QuestModal
-                size={80}
-                icon={Icon}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_center-line--rectangle event-map_center-line--rectangle_seventh">
-              {" "}
-              <QuestModal
-                size={80}
-                icon={Icon}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
-            <div className="event-map_center-line--rectangle event-map_center-line--rectangle_eighth">
-              {" "}
-              <QuestModal
-                size={80}
-                icon={Icon}
-                title={"Share Your Feat!"}
-                text={
-                  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est error, minus molestias omnis adipisci porro magni natus libero, obcaecati ab eius unde sequi molestiae. Repellat ut eligendi aperiam minima voluptatibus."
-                }
-                video={"Video 1"}
-              />
-            </div>
+            {data.itemsCenter.map((item, index) => {
+              return (
+                <div className={item.className}>
+                  <QuestModal
+                    size={item.size}
+                    icon={iconComponents[item.icon]}
+                    title={item.title}
+                    text={item.text}
+                    video={item.videoName}
+                  />
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="event-map_bottom-container">
+            {data.itemsBottom.map((item, index) => {
+              return (
+                <div className={item.className}>
+                  <QuestModal
+                    size={item.size}
+                    icon={iconComponents[item.icon]}
+                    title={item.title}
+                    text={item.text}
+                    video={item.videoName}
+                  />
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
